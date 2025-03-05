91b41489d72de546f1ed87e2a30c7e534036fbc5
NatAn <andrei.v.0204@gmail.com>
2025-03-05 10:47:13 +0300

    Созданы шаблон счетчика и кнопка

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -8,12 +8,17 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var countImageView: UILabel!
+    
+    @IBOutlet weak var changeButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func buttonDidTap(_ sender: Any) {
+        print("Tap")
+    }
+    
 }
 
